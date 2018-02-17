9b76d56a2d9be9e379e2dc573ea48de34dd40f5e
kingshukb <kingshukb@shopsense.co>
2018-02-17 13:18:49 +0530

    initial setup

---
+++ client.py
@@ -0,0 +1,9 @@
+
+from constants.urls import URL
+
+class Client:
+    """Razorpay client class"""
+
+    DEFAULTS = {
+        'base_url': URL.BASE_URL
+    }
---
+++ constants/urls.py
@@ -0,0 +1,5 @@
+
+
+
+class URL(object):
+    BASE_URL = 'https://sandbox-approvals-api.getsimpl.com/api/v2'
